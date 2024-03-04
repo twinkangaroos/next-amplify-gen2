d0948af75c37a2b8c1f818f7d563fab2c0f52dc4
twinkangaroos <twinkangaroos@gmail.com>
2024-03-04 13:27:07 +0900

    Add Login css.

--- components/Login.tsx
+++ components/Login.tsx
@@ -4,6 +4,7 @@ import { withAuthenticator } from "@aws-amplify/ui-react"
 import { AuthUser } from "aws-amplify/auth"
 import { redirect } from "next/navigation"
 import { useEffect } from "react"
+import '@aws-amplify/ui-react/styles.css'
 
 function Login({ user }: { user?: AuthUser }) {
     useEffect(() => {
